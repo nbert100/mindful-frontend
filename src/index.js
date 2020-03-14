975b3c38ab636b386c7f9a47acf28d4037e9aec0
nbert100 <nbert14@gmail.com>
2020-03-14 16:40:24 -0400

    import files into index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+//provider wraps the app so that we can use redux. any component  we wrap in provider will have access to the redux store
 // import './index.css';
 import App from './App';
 // set up store so we need to import create store
