ff4c89e1de19833e8ccc6e98fd473d68f5cb900b
nbert100 <nbert14@gmail.com>
2020-03-14 15:04:15 -0400

    add react stuff before dropping db

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux';
+import thunk from 'redux-thunk'
+// import './index.css';
 import App from './App';
-
+// set up store so we need to import create store
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
