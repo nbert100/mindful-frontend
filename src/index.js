583496cc7c49a56754f049ea356bfa88d9d983a3
nbert100 <nbert14@gmail.com>
2020-03-14 21:46:27 -0400

    create store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 //provider wraps the app so that we can use redux. any component  we wrap in provider will have access to the redux store
@@ -8,6 +8,16 @@ import { Provider } from 'react-redux'
 import App from './App';
 // set up store so we need to import create store
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+//store is where you're storing your data globally
+
+ReactDOM.render(
+    <Provider store={store}>
+     <App />   
+    </Provider>, 
+document.getElementById('root')
+);
 
 
