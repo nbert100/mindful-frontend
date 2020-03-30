b623c9c5b42e5586a6b7f32d20c235bae9540074
nbert100 <nbert14@gmail.com>
2020-03-30 02:07:18 -0400

    added bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,8 @@
-import React from "react";
-import { Route, Link } from "react-router-dom";
+// import React from "react";
+// import { Route, Link } from "react-router-dom";
+
+// Home (list of clients)
+// Back
+// New Client
+// New Provider
+//Providers
