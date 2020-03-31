39aefc8de9c7920ee655b1ba41575639b9bf3d52
nbert100 <nbert14@gmail.com>
2020-03-31 01:47:11 -0400

    separate store file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { logger } from "redux-logger";
-//thunk returns function from the action creator
+import "./index.css";
 import { Provider } from "react-redux";
-import rootReducer from "./reducers/rootReducer";
 //provider wraps the app so that we can use redux. any component  we wrap in provider will have access to the redux store
-// import './index.css';
+
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
+import store from "./store";
 // set up store so we need to import create store
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk, logger))
-);
 //store is where you're storing your data globally
 
 ReactDOM.render(
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { logger } from "redux-logger";
+import rootReducer from "./reducers/rootReducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+export default store;
