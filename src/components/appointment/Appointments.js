ddb587eb41ff3271e146bca0a3869c22fa36d844
nbert100 <nbert14@gmail.com>
2020-04-04 14:02:49 -0400

    add provyders to appointment list

--- src/components/appointment/Appointments.js
+++ src/components/appointment/Appointments.js
@@ -6,21 +6,28 @@ import moment from "moment";
 import { connect } from "react-redux";
 import { removeAppointment } from "../../actions/removeAppointment";
 
-const Appointments = props => {
-  const formatTime = time => moment(time).format("MMMM Do YYYY, h:mm a");
-
-  const removeAppointment = appointment => {
-    props.removeAppointment(appointment);
+const Appointments = (props) => {
+  const formatTime = (time) => moment(time).format("MMMM Do YYYY, h:mm a");
+  const thisProvyder = (appointment) => {
+    let appProv = props.provyders.find(
+      (provyder) => provyder.id === appointment.provyder_id
+    );
+    return appProv;
   };
 
+  const removeAppointment = (appointment) => {
+    props.removeAppointment(appointment);
+  };
   return (
     <div className="appointment-container">
       <h5>Appointments:</h5>
       <br></br>
       {props.appointments &&
-        props.appointments.map(appointment => (
+        props.appointments.map((appointment) => (
           <li key={appointment.id}>
             {appointment.kind} appointment on {formatTime(appointment.when)}{" "}
+            with Dr.{thisProvyder(appointment).last_name},{" "}
+            {thisProvyder(appointment).specialty}
             <span
               aria-label="and x"
               role="img"
